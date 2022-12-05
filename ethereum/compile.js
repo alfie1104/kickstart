94d51b8fdf0d22b77dc4f0a66c3f22cf388b09f3
peter_alfie <alfie1104@gmail.com>
2022-12-06 08:51:47 +0900

    feat(compile): setting up compile file

---
+++ ethereum/compile.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const solc = require("solc"); //solidity compiler
+const fs = require("fs-extra");
+
+//1. Delete entire 'build' folder
+const buildPath = path.resolve(__dirname, "build"); //current working directory/build
+fs.removeSync(buildPath);
+
+//2. Read 'Campaign.sol' from the 'contracts' folder
+const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaignPath, "utf8");
+
+//3. Compile both contracts with solidity compiler
+const output = solc.compile(source, 1).contracts;
+
+//4. Write output to the 'build' directory
+fs.ensureDirSync(buildPath); //create build folder
+
+for (let contract in output) {
+  fs.outputJSONSync(
+    path.resolve(buildPath, `${contract.replace(":", "")}.json`),
+    output[contract] //actual content that I want to write in json file
+  );
+}
