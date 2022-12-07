c17a1825b68fcfe2699b0d3b34b957cbe4a3c440
peter_alfie <alfie1104@gmail.com>
2022-12-08 08:46:56 +0900

    feat(web3.js): setting up HttpProvider incase of not existing metamask or serverside rendering

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import factory from "../ethereum/factory";
 
 export default () => {
+  useEffect(async () => {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+
+    console.log(campaigns);
+  }, []);
+
   return <h1>This is the campaign list page!!</h1>;
 };
+
+// class CampaignIndex extends React.Component {
+//   async componentDidMount(){
+//     const campaigns = await factory.methods.getDeployedCampaigns().call();
+
+//     console.log(campaigns);
+//   }
+
+//   render(){
+//     return <div>Campaign Index!</div>
+//   }
+// }
+
+// export default CampaignIndex;
