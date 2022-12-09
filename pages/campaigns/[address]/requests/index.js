dc12c94a0a8085c2a8fd28ca6ecae119dbd088d4
peter_alfie <alfie1104@gmail.com>
2022-12-09 14:14:10 +0900

    feat(campaigns/[address]/requests/index): fetching requests data

--- pages/campaigns/[address]/requests/index.js
+++ pages/campaigns/[address]/requests/index.js
@@ -2,22 +2,37 @@ import React from "react";
 import Link from "next/link";
 import { Button } from "semantic-ui-react";
 import Layout from "../../../../components/Layout";
+import Campaign from "../../../../ethereum/campaign";
 
 const Requests = (props) => {
+  console.log(props.requests);
+
   return (
     <Layout>
       <h3>Requests</h3>
       <Link href={`/campaigns/${props.address}/requests/new`}>
-        <Button primary>Add Request</Button>
+        <Button primary floated="right">
+          Add Request
+        </Button>
       </Link>
     </Layout>
   );
 };
 
-Requests.getInitialProps = (ctx) => {
+Requests.getInitialProps = async (ctx) => {
   const { address } = ctx.query;
+  const campaign = Campaign(address);
+  const requestCount = await campaign.methods.getRequestsCount().call();
 
-  return { address };
+  const requests = await Promise.all(
+    Array(parseInt(requestCount))
+      .fill()
+      .map((element, index) => {
+        return campaign.methods.requests(index).call();
+      })
+  );
+
+  return { address, requests, requestCount };
 };
 
 export default Requests;
