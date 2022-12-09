3bcfa123f2dae4eca4bccdb2dd3a8304fa8e738f
peter_alfie <alfie1104@gmail.com>
2022-12-09 14:35:41 +0900

    feat(components/requestrow): approving a request

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { Table } from "semantic-ui-react";
+import { Table, Button } from "semantic-ui-react";
 import web3 from "../ethereum/web3";
+import Campaign from "../ethereum/campaign";
 
 const RequestRow = ({ request, address, id, approversCount }) => {
   const { Row, Cell } = Table;
   const { description, value, recipient, approvalCount } = request;
+
+  const onApprove = async (event) => {
+    event.preventDefault();
+
+    const campaign = Campaign(address);
+    const accounts = await web3.eth.getAccounts();
+
+    await campaign.methods.approveRequest(id).send({
+      from: accounts[0],
+    });
+  };
+
+  const onFinalize = (event) => {
+    event.preventDefault();
+
+    console.log("Finalize");
+  };
 
   return (
     <Row>
@@ -15,8 +33,16 @@ const RequestRow = ({ request, address, id, approversCount }) => {
       <Cell>
         {approvalCount}/{approversCount}
       </Cell>
-      <Cell>Approve</Cell>
-      <Cell>Finalize</Cell>
+      <Cell>
+        <Button color="green" basic onClick={onApprove}>
+          Approve
+        </Button>
+      </Cell>
+      <Cell>
+        <Button color="red" basic onClick={onFinalize}>
+          Finalize
+        </Button>
+      </Cell>
     </Row>
   );
 };
