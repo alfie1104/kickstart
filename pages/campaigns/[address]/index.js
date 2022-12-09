c722d46987cc425994ed9a7bdc3a567bb95ae2ed
peter_alfie <alfie1104@gmail.com>
2022-12-09 10:47:44 +0900

    feat(ethereum/campaign, pages/capaigns/index): accessing a campaign

--- pages/campaigns/[address]/index.js
+++ pages/campaigns/[address]/index.js
@@ -1,15 +1,25 @@
 import { useRouter } from "next/router";
 import Layout from "../../../components/Layout";
+import Campaign from "../../../ethereum/campaign";
 
 const CampaignShow = (props) => {
   const router = useRouter();
 
   const { address } = router.query;
+
+  console.log(props.summary);
   return (
     <Layout>
       <h3>Campaign Show</h3>
     </Layout>
   );
+};
+
+CampaignShow.getInitialProps = async (ctx) => {
+  const campaign = Campaign(ctx.query.address);
+  const summary = await campaign.methods.getSummary().call();
+
+  return { summary };
 };
 
 export default CampaignShow;
