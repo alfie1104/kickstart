4d595fc47a0f33c3cabd7df11eb68eba5f0ae8ce
peter_alfie <alfie1104@gmail.com>
2022-12-09 11:02:03 +0900

    feat(campaigns/address/index): displaying campaign properties

--- pages/campaigns/[address]/index.js
+++ pages/campaigns/[address]/index.js
@@ -1,6 +1,7 @@
 import { Card } from "semantic-ui-react";
 import Layout from "../../../components/Layout";
 import Campaign from "../../../ethereum/campaign";
+import web3 from "../../../ethereum/web3";
 
 const CampaignShow = (props) => {
   const renderCards = () => {
@@ -19,6 +20,30 @@ const CampaignShow = (props) => {
         description:
           "The manager created this campaign and can create requests to withdraw money",
         style: { overflowWrap: "break-word" },
+      },
+      {
+        header: minimumContribution,
+        meta: "Minimum Contribution (wei)",
+        description:
+          "You must contribute at least this much wei to become an approver",
+      },
+      {
+        header: requestsCount,
+        meta: "Number of Requests",
+        description:
+          "A request tries to withdraw money from the contract. Requests must be approved by approvers",
+      },
+      {
+        header: approversCount,
+        meta: "Number of Approvers",
+        description:
+          "Number of people who have already donated to this campaign",
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ether)",
+        description:
+          "The balance is how much money this campaign has left to spend.",
       },
     ];
 
