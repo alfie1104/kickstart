6b69828085f9a7f085d301fb8aa4082ca257a35d
peter_alfie <alfie1104@gmail.com>
2022-12-09 13:56:12 +0900

    feat(campaigns/[address]/requests/new): creating a request

--- pages/campaigns/[address]/requests/new.js
+++ pages/campaigns/[address]/requests/new.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { Button, Form, Input, Message } from "semantic-ui-react";
 import Layout from "../../../../components/Layout";
@@ -5,18 +7,42 @@ import Campaign from "../../../../ethereum/campaign";
 import web3 from "../../../../ethereum/web3";
 
 const RequestNew = (props) => {
+  const router = useRouter();
+
   const [description, setDescription] = useState("");
   const [value, setValue] = useState("");
   const [recipient, setRecipient] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+
+    const campaign = Campaign(props.address);
+    setLoading(true);
+    setErrorMessage("");
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods
+        .createRequest(description, web3.utils.toWei(value, "ether"), recipient)
+        .send({
+          from: accounts[0],
+        });
+
+      router.push(`/campaigns/${props.address}/requests`);
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+
+    setLoading(false);
   };
 
   return (
     <Layout>
+      <Link href={`/campaigns/${props.address}/requests`}>
+        <a>Back</a>
+      </Link>
       <h3>Create a Request</h3>
       <Form onSubmit={onSubmit} error={!!errorMessage}>
         <Form.Field>
@@ -37,10 +63,10 @@ const RequestNew = (props) => {
             onChange={(e) => setRecipient(e.target.value)}
           />
         </Form.Field>
+        <Message error header="Oops!" content={errorMessage} />
         <Button primary loading={loading} disabled={loading}>
           Create!
         </Button>
-        <Message error></Message>
       </Form>
     </Layout>
   );
