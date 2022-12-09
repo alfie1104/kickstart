ba8cf5180b441fcc8677b2d05bcb75f3b085da24
peter_alfie <alfie1104@gmail.com>
2022-12-09 14:30:22 +0900

    feat(components/requestrow): create request row contents

--- pages/campaigns/[address]/requests/index.js
+++ pages/campaigns/[address]/requests/index.js
@@ -1,20 +1,47 @@
 import React from "react";
 import Link from "next/link";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import Layout from "../../../../components/Layout";
 import Campaign from "../../../../ethereum/campaign";
+import RequestRow from "../../../../components/RequestRow";
 
 const Requests = (props) => {
-  console.log(props.requests);
+  const { Header, Row, HeaderCell, Body } = Table;
+
+  const renderRows = () => {
+    return props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          request={request}
+          address={props.address}
+          approversCount={props.approversCount}
+          id={index + 1}
+        />
+      );
+    });
+  };
 
   return (
     <Layout>
       <h3>Requests</h3>
       <Link href={`/campaigns/${props.address}/requests/new`}>
-        <Button primary floated="right">
-          Add Request
-        </Button>
+        <Button primary>Add Request</Button>
       </Link>
+      <Table>
+        <Header>
+          <Row>
+            <HeaderCell>ID</HeaderCell>
+            <HeaderCell>Description</HeaderCell>
+            <HeaderCell>Amount</HeaderCell>
+            <HeaderCell>Recipient</HeaderCell>
+            <HeaderCell>Approval Count</HeaderCell>
+            <HeaderCell>Approve</HeaderCell>
+            <HeaderCell>Finalize</HeaderCell>
+          </Row>
+        </Header>
+        <Body>{renderRows()}</Body>
+      </Table>
     </Layout>
   );
 };
@@ -23,6 +50,7 @@ Requests.getInitialProps = async (ctx) => {
   const { address } = ctx.query;
   const campaign = Campaign(address);
   const requestCount = await campaign.methods.getRequestsCount().call();
+  const approversCount = await campaign.methods.approversCount().call();
 
   const requests = await Promise.all(
     Array(parseInt(requestCount))
@@ -32,7 +60,7 @@ Requests.getInitialProps = async (ctx) => {
       })
   );
 
-  return { address, requests, requestCount };
+  return { address, requests, requestCount, approversCount };
 };
 
 export default Requests;
